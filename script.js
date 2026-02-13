dbc94ea64d0da3f49edbfd91cdd42180d0b98ec7
Namdepzai391 <manpro391@gmail.com>
2026-02-13 16:11:07 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function showImage(img) {
+  document.getElementById("imageDescription").innerText = img.alt;
+}
+
+function resetImage() {
+  document.getElementById("imageDescription").innerText =
+    "Hover over or focus on an image to see the description.";
+}
+
+function addTabIndex() {
+  console.log("Page loaded — adding tabindex");
+
+  let images = document.querySelectorAll(".gallery img");
+
+  for (let i = 0; i < images.length; i++) {
+    images[i].setAttribute("tabindex", "0");
+  }
+}
